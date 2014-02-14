f7da0449cfd97389d81da0bdff00c97bdcb907f0
Mr. Spothawk <mr.spothawk@gmail.com>
2014-02-14 15:28:21 -0800

    force cookies to only be sent via HTTPS

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var https   = require('https');
 var path    = require('path');
 var app     = express();
 var db      = require('./db.js');
+
+
 
 app.set('port', process.env.PORT || 3500);
 app.use(express.favicon());
@@ -10,7 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
-app.use(express.session({ secret: process.env.SESSION_SECRET || 'keyboardcat' }));
+app.use(express.session({ 
+  secret: process.env.SESSION_SECRET || 'keyboardcat' ,
+  cookie: {httpOnly: true, secure: true}
+}));
+
 app.use(express.static(path.join(__dirname, '..', '/public')));
 
 
