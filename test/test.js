107b4522bbe5632280a8d8f856aeb16a8acb8f61
Mr. Spothawk <mr.spothawk@gmail.com>
2014-02-20 16:11:21 -0800

    running "npm test" now clears the specified test-db between tests

--- test/test.js
+++ test/test.js
@@ -6,10 +6,10 @@ var MONGODB_URL = process.env.MONGODB_URL ||
 var expect   = require('chai').expect,
     assert   = require('assert'),
     mongoose = require('mongoose'),
-    db       = require('../server/db.js'),
+    db       = require('../server/db.js');
     clearDB  = require('mocha-mongoose')(MONGODB_URL);
 
-
+var mongo;
 
 /* DB tests */
 describe('#DB', function(){
@@ -17,31 +17,35 @@ describe('#DB', function(){
   beforeEach(function(done){
     if (mongoose.connection.db) return done();
     
-    mongoose.connect(MONGODB_URL, done);
-    // mongo.clear();
+    mongo = mongoose.connect(MONGODB_URL);
+    clearDB(done);
+    // done();
   });
   describe('schema functions', function(){
     var models = require('../server/schema.js'),
-      note1 = new models.Note ({title: "n1", tags: ["note", "1", "tag"], body: "note 1 is this"}),
-      note2 = new models.Note ({title: "n2", tags: ["note", "2", "tag"], body: "note 2 is this"}),
-      note3 = new models.Note ({title: "n3", tags: ["note", "3", "tag"], body: "note 3 is this"});
+      note1 = {title: "n1", tags: ["note", "1", "tag"], body: "note 1 is this"},
+      note2 = {title: "n2", tags: ["note", "2", "tag"], body: "note 2 is this"},
+      note3 = {title: "n3", tags: ["note", "3", "tag"], body: "note 3 is this"};
     
     it('should save notes without error',function(done){
-      note1.save(done);
-        // test save: 
-        // curl --insecure -X POST -H "Content-Type: application/json" -d '{"title": "hi", "tags": ["help", "me", "save"], "body": "longbodybodfkjsdf lkjsdflkjsdf sdflkjsdf "}' https://localhost:3030/notes/
+      var promise = models.Note.create([note1, note2, note3]);
+      promise.then(function(){
+        done();
+      }); 
     });
 
-    it('should retrieve a note when requested by ID#',function(){});
-        // test retrieve: 
-        // curl --insecure -X GET  https://localhost:3030/notes/52f5e3a2efa5d2c5b57c76b4
-    it('should return a list of all notes',function(){});
-        // test getNoteList: 
-        // curl --insecure -X GET  https://localhost:3030/notes/list
-    it('should destroy all notes',function(){});
-        // testring destroy: 
-        // curl --insecure -X GET  https://localhost:3030/notes/destroy/53046abcf5df756927fd01ac
+    // it('should retrieve a note when requested by ID#',function(){});
+    // it('should return a list of all notes',function(){});
+    // it('should destroy all notes',function(){});
   });
   // after(); /* We should now drop the testing DB */
 
 });
+        // test save: 
+        // curl --insecure -X POST -H "Content-Type: application/json" -d '{"title": "hi", "tags": ["help", "me", "save"], "body": "longbodybodfkjsdf lkjsdflkjsdf sdflkjsdf "}' https://localhost:3030/notes/
+        // testring destroy: 
+        // curl --insecure -X GET  https://localhost:3030/notes/destroy/53046abcf5df756927fd01ac
+        // test getNoteList: 
+        // curl --insecure -X GET  https://localhost:3030/notes/list
+        // test retrieve: 
+        // curl --insecure -X GET  https://localhost:3030/notes/52f5e3a2efa5d2c5b57c76b4
