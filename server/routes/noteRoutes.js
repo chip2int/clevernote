0903eb2c35c57d2c0fbf5f2c16ae086b6fd7c894
Mr. Spothawk <mr.spothawk@gmail.com>
2014-02-28 11:07:44 -0800

    corrected route for updating new notes

--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -4,18 +4,18 @@ var noteCtrl = require('../controllers/noteController.js');
 var userCtrl = require('../controllers/userController');
 
 module.exports = function(app){
-  
-  app.get('/notes/new'              , noteCtrl.newNote);
+
+  // app.get('/notes/new'              , noteCtrl.newNote);
+  app.post('/notes/new'             , noteCtrl.updateNote);
   app.get('/notes/destroy/:note_id' , noteCtrl.destroyNote );
   app.post('/notes/'                , noteCtrl.updateNote );
   app.post('/notes/tags/:note_id'   , noteCtrl.updateTags );
-  
+
   //TODO: move the following to to userRoutes.js
-  app.get('/notes/list'             , userCtrl.listUsersNotes ); 
+  app.get('/notes/list'             , userCtrl.listUsersNotes );
 
 
 
 
   // app.get('/notes/:note_id'         , noteCtrl.retrieveNote );
 };
-
